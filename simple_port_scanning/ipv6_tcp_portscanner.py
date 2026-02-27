d9c054380ea39e95a8c63bb24812713f8bd600f6
Saymon Hasan <161878824+Samstatix@users.noreply.github.com>
2026-02-27 13:46:20 +0000

    commit

--- simple_port_scanning/ipv6_tcp_portscanner.py
+++ simple_port_scanning/ipv6_tcp_portscanner.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python2
 import socket
 
-host = "::1"          # IPv6 loopback
+host = "::1"          
 port = 443
 
 sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
 def portscanner(port):
-    # For IPv6, connect_ex expects a tuple (address, port, flowinfo, scopeid)
     if sock.connect_ex((host, port, 0, 0)) == 0:
         print "Port %d is OPEN" % port
     else:
         print "Port %d is CLOSED" % port
 
 portscanner(port)
-sock.close()
