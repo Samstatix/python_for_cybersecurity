087deafe154317f2862158f4cf50b42e6f576c52
Saymon Hasan <161878824+Samstatix@users.noreply.github.com>
2026-03-18 00:14:25 +0600

    Migrate ipv6_tcp_portscanner to Python 3
    Updated the script to use Python 3 and accept user input for host and port.

--- simple_port_scanning/ipv6_tcp_portscanner.py
+++ simple_port_scanning/ipv6_tcp_portscanner.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import socket
 
-host = "::1"          
-port = 443
+host = input("Enter the IP address of the host: ")      
+port = int(input("Enter the port: "))
 
 sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
 def portscanner(port):
     if sock.connect_ex((host, port, 0, 0)) == 0:
-        print "Port %d is OPEN" % port
+        print ("Port %d is OPEN" % port)
     else:
-        print "Port %d is CLOSED" % port
-
+        print ("Port %d is CLOSED" % port)
+    sock.close()
 portscanner(port)
