1ab621bc04efdb2ed0f8305fe1ee80f143841f54
Saymon Hasan <161878824+Samstatix@users.noreply.github.com>
2026-03-17 23:52:51 +0600

    Update script to use Python 3 syntax

--- simple_port_scanning/ipv4_tcp_portscanneer.py
+++ simple_port_scanning/ipv4_tcp_portscanneer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import socket
 
@@ -9,8 +9,8 @@ port = 443
 
 def portscanner(port):
     if sock.connect_ex((host, port)):
-        print "Port %d is closed" % (port)
+        print ("Port %d is closed" % port)
     else:
-        print "Port %d is opened" % (port)
+        print ("Port %d is opened" % port)
 
 portscanner(port)
